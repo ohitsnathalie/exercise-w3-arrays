b43bbeb1ace9299002110e178f45d028359f36ba
Nathalie <nathae28@hotmail.com>
2024-02-17 15:52:00 +0100

    solved some of the exercises

--- src/index.js
+++ src/index.js
@@ -1,64 +1,82 @@
 const pokemons = [
-  "Bulbasaur",
-  "Ivysaur",
-  "Venusaur",
-  "Charmander",
-  "Charmeleon",
-  "Charizard",
-  "Squirtle",
-  "Wartortle",
-  "Blastoise",
-  "Metapod",
-  "Weedle",
-  "Pikachu",
-  "Pidgey"
+	"Bulbasaur",
+	"Ivysaur",
+	"Venusaur",
+	"Charmander",
+	"Charmeleon",
+	"Charizard",
+	"Squirtle",
+	"Wartortle",
+	"Blastoise",
+	"Metapod",
+	"Weedle",
+	"Pikachu",
+	"Pidgey",
 ];
 
 const countThem = () => {
-  // print out, in a sentence, how many pokemons I have.
-  // like: "I have x pokemons!"
+	// print out, in a sentence, how many pokemons I have.
+	// like: "I have x pokemons!"
+	let length = pokemons.length;
+	console.log(`I have ${pokemons.length} pokemons`);
 };
+countThem();
 
 const orderThem = () => {
-  // order the pokemons alphabetically
+	// order the pokemons alphabetically
+	let order = pokemons.toSorted();
+	console.log(order);
 };
+orderThem();
 
 const flipThem = () => {
-  // reverse the order of the pokemons
+	// reverse the order of the pokemons
+	let reverse = pokemons.reverse();
+	console.log(reverse);
 };
+flipThem();
 
 const makeThemBig = () => {
-  // print the pokemons in UPPERCASE letters
+	// print the pokemons in UPPERCASE letters
+	const caps = pokemons.map((pokemon) => pokemon.toUpperCase());
+	console.log(caps);
 };
+makeThemBig();
 
 const onlyTheBs = () => {
-  // only print the pokemons that starts with B
+	// only print the pokemons that starts with B
+	const startingWithB = pokemons.filter((pokemon) => pokemon.startsWith("B"));
+	console.log(startingWithB);
 };
+onlyTheBs();
 
 const notTheCs = () => {
-  // remove all pokemons that starts with C
+	// remove all pokemons that starts with C
+	const withoutC = pokemons.filter((pokemon) => !pokemon.startsWith("C"));
+	console.log(withoutC);
 };
+notTheCs();
 
 const nameAndIdThanks = () => {
-  // print out name and index of all pokemons
-  // like: number x - Squirtle
+	// print out name and index of all pokemons
+	// like: number x - Squirtle
 };
 
-const catchPokemon = name => {
-  // add a pokemon with a name of your choice to the list,
-  // print the list so you see its there.
+const catchPokemon = (name) => {
+	// add a pokemon with a name of your choice to the list,
+	// print the list so you see its there.
 };
 
-const didICatchIt = name => {
-  // check the pokemons to see if a specific pokemon is in the array
+const didICatchIt = (name) => {
+	// check the pokemons to see if a specific pokemon is in the array
 };
 
 const addInThirdPlace = () => {
-  // add the pokemon "Clefairy" in the third place of the array
-  // print the list so you see its there.
+	// add the pokemon "Clefairy" in the third place of the array
+	// print the list so you see its there.
 };
 
 // ***BONUS***
 const theLongestName = () => {
-  // find the pokemon with the longest name
+	// find the pokemon with the longest name
 };
